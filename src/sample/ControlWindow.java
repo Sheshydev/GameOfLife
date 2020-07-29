445295564e0cf4bac23abbfe340e16ed450d4ea3
Thomas <thomas1122@gmail.com>
2020-07-29 15:38:58 +0100

    control window added

---
+++ src/sample/ControlWindow.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class ControlWindow extends GridPane {
+
+    public ControlWindow(){
+        this.setStyle("-fx-background-color: #b5dcff");
+        this.setWidth(Main.SCREENWIDTH);
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,79 +13,73 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
-    public static int WIDTH = 306;
-    public static int HEIGHT = 180;
+    public static int WIDTH = 306; //amount of cells in a row
+    public static int HEIGHT = 180; //amount of cells in a column
     public static double SCREENWIDTH = 1920;
     public static double SCREENHEIGHT = 1080;
+
     private static Cell cellArr[][] = new Cell[HEIGHT][WIDTH];
-    private Pane root = new Pane();
-    private Group group = new Group();
-    private double yMoveSpeed = 5;
-    private double xMoveSpeed = 5;
+    private GridPane root = new GridPane();
+    private Pane boardScene = new Pane();
+    private Group board = new Group();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        final double SCALE_DELTA = 1.1;
-        createBoard();
+        Scene scene = new Scene(root, SCREENWIDTH, SCREENHEIGHT);
+        primaryStage.setTitle("Game of Life");
+        primaryStage.setScene(scene);
 
-        root.setOnScroll(new EventHandler<ScrollEvent>() {
-            @Override public void handle(ScrollEvent event) {
-                event.consume();
+        final double SCALE_DELTA = 1.05; //zoom factor
 
-                if (event.getDeltaY() == 0) {
-                    return;
-                }
+        boardScene.setOnScroll(event -> {
+            //zoom function
+            event.consume();
 
-                double scaleFactor =
-                        (event.getDeltaY() > 0) ? SCALE_DELTA : 1/SCALE_DELTA;
-
-                group.setScaleX(group.getScaleX() * scaleFactor);
-                group.setScaleY(group.getScaleY() * scaleFactor);
+            if (event.getDeltaY() == 0) {
+                return;
             }
+
+            double scaleFactor = (event.getDeltaY() > 0) ? SCALE_DELTA : 1/SCALE_DELTA;
+
+            board.setScaleX(board.getScaleX() * scaleFactor);
+            board.setScaleY(board.getScaleY() * scaleFactor);
         });
 
         final Delta dragDelta = new Delta();
-        group.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
-                // record a delta distance for the drag and drop operation.
-                dragDelta.x = group.getTranslateX() - mouseEvent.getScreenX();
-                dragDelta.y = group.getTranslateY() - mouseEvent.getScreenY();
-            }
+        board.setOnMousePressed(mouseEvent -> {
+            // record a delta distance for the drag and drop operation.
+            dragDelta.x = board.getTranslateX() - mouseEvent.getScreenX();
+            dragDelta.y = board.getTranslateY() - mouseEvent.getScreenY();
         });
-        group.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
-                group.setTranslateX(mouseEvent.getScreenX() + dragDelta.x);
-                group.setTranslateY(mouseEvent.getScreenY() + dragDelta.y);
-                /*primaryStage.setX(mouseEvent.getScreenX() + dragDelta.x);
-                primaryStage.setY(mouseEvent.getScreenY() + dragDelta.y);*/
-            }
+        board.setOnMouseDragged(mouseEvent -> {
+            //drags the boardScene
+            board.setTranslateX(mouseEvent.getScreenX() + dragDelta.x);
+            board.setTranslateY(mouseEvent.getScreenY() + dragDelta.y);
         });
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.05), actionEvent -> {
             for(int y = 0; y < HEIGHT; y++){
                 for(int x = 0; x < WIDTH; x++){
+                    //all cells scan their neighbors
                     cellArr[y][x].scanState();
                 }
             }
             for(int y = 0; y < HEIGHT; y++){
                 for(int x = 0; x < WIDTH; x++){
+                    //all cells update their state after scanning
                     cellArr[y][x].updateState();
                 }
             }
         }));
-
-        root.getChildren().add(group);
-        timeline.setCycleCount(Animation.INDEFINITE);
-        Scene scene = new Scene(root, SCREENWIDTH, SCREENHEIGHT);
-        primaryStage.setTitle("Game of Life");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
         scene.setOnKeyPressed(e -> {
             if(e.getCode() == KeyCode.SPACE){
                 if(timeline.getStatus() == Animation.Status.RUNNING)
@@ -94,6 +88,24 @@ public class Main extends Application {
                     timeline.play();
             }
         });
+        timeline.setCycleCount(Animation.INDEFINITE);
+
+        //divides control window and boardScene
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col1.setPercentWidth(85);
+        col2.setPercentWidth(15);
+        root.getColumnConstraints().add(col1);
+        root.getColumnConstraints().add(col2);
+        root.addColumn(0,boardScene);
+        ControlWindow control = new ControlWindow();
+        root.addColumn(1);
+        root.getChildren().remove(control);
+
+        createBoard();
+        boardScene.getChildren().add(board);
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
@@ -105,7 +117,7 @@ public class Main extends Application {
             for(int x = 0; x < WIDTH; x++){
                 Cell cell = new Cell(y, x);
                 cellArr[y][x] = cell;
-                group.getChildren().add(cell);
+                board.getChildren().add(cell);
             }
         }
     }
@@ -114,5 +126,5 @@ public class Main extends Application {
         return cellArr[y][x];
     }
 
-    class Delta { double x, y; }
+    class Delta { double x, y; } //records cursor coordinates
 }
